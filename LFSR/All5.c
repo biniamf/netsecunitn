2af295273c5906d51a62245521bc75d5c929629a
biniamf <biniam@ymail.com>
2013-11-20 22:09:40 +0100

    works

--- LFSR/All5.c
+++
@@ -1,9 +0,0 @@
-/*
- * All5.c
- *
- *  Created on: Nov 17, 2013
- *      Author: biniam
- */
-
-
-#include "All5.h"
--- LFSR/Maj5.c
+++
@@ -1,9 +0,0 @@
-/*
- * Maj5.c
- *
- *  Created on: Nov 17, 2013
- *      Author: biniam
- */
-
-
-#include "Maj5.h"
